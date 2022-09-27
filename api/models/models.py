88eed83ce8fa1a4cb6ae574db4950e8582f4a0c0
Anne <92945218+vian-ai@users.noreply.github.com>
2022-09-27 12:21:32 -0600

    edited model fields

--- api/models/models.py
+++ api/models/models.py
@@ -2,46 +2,42 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 class Characters(models.Model):
-    name_en = models.CharField(max_length=255)
-    name_jp = models.CharField(max_length=255)
+    name_en = models.CharField(max_length=50, blank=False, null=False, default='N/A')
+    name_jp = models.CharField(max_length=50, blank=False, null=False, default='N/A')
     age = models.IntegerField()
-    birthdate = models.CharField(max_length=255)
-    zodiac = models.CharField(max_length=255)
-    affiliations = models.CharField(max_length=255)
-    title = models.CharField(max_length=255)
-    height = models.CharField(max_length=255)
-    weight = models.CharField(max_length=255)
-    image = models.URLField(max_length=1024, null=True, blank=True)
-
-    def __str__(self):
-        return self.name_en
-
-
-class Units(models.Model):
-    name_en = models.CharField(max_length=255)
-    name_jp = models.CharField(max_length=255)
-    model_type = models.CharField(max_length=255)
-    pilot = models.CharField(max_length=255)
-    soul = models.CharField(max_length=255)
+    birthdate = models.CharField(max_length=50, blank=False, null=False, default='N/A')
+    zodiac = models.CharField(max_length=50, blank=False, null=False, default='N/A')
+    affiliations = models.CharField(max_length=50, blank=False, null=False, default='N/A')
+    title = models.CharField(max_length=50, blank=False, null=False, default='N/A')
+    height = models.CharField(max_length=50, blank=False, null=False, default='N/A')
+    weight = models.CharField(max_length=50, blank=False, null=False, default='N/A')
     image = models.URLField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return self.name_en
     
+class EvaUnits(models.Model):
+    eva_en = models.CharField(max_length=50, blank=False, null=False, default='N/A')
+    eva_jp = models.CharField(max_length=50, blank=False, null=False, default='N/A')
+    model_type = models.CharField(max_length=50, blank=False, null=False, default='N/A')
+    pilot = models.CharField(max_length=50, blank=False, null=False, default='N/A')
+    soul = models.CharField(max_length=50, blank=False, null=False, default='N/A')
+    image = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.eva_en
+
+
 class Evangelion(models.Model):
-    title_en = models.CharField(max_length=255)
-    title_jp = models.CharField(max_length=255)
-    type = models.CharField(max_length=255)
+    title_en = models.CharField(max_length=50, blank=False, null=False, default='N/A')
+    title_jp = models.CharField(max_length=50, blank=False, null=False, default='N/A')
+    type = models.CharField(max_length=50, blank=False, null=False, default='N/A')
     episodes = models.IntegerField()
-    genre = ArrayField(
-        ArrayField(
-            models.CharField(max_length=255)
-        )
-    )
-    released = models.CharField(max_length=255)
+    genre = ArrayField(models.CharField(max_length=255, null=True))
+    released = models.CharField(max_length=50, blank=False, null=False, default='N/A')
     logo = models.URLField(max_length=1024, null=True, blank=True)
-    characters = models.ManyToManyField(Characters)
-    evangelions = models.ManyToManyField(Units)
-    
+    characters = models.ManyToManyField(Characters, blank=True, related_name="Chars")
+    units = models.ManyToManyField(EvaUnits, blank=True, related_name="Evas")
+
     def __str__(self):
         return self.title_en
